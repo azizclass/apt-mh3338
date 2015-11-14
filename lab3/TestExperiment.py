4a278cc3c6e3b2afa28dade604f3f58763984206
Matthew Halpern <matthalp@utexas.edu>
2015-11-13 18:34:27 -0600

    Add test to increase coverage

--- lab3/TestExperiment.py
+++ lab3/TestExperiment.py
@@ -12,8 +12,8 @@ class MyTestCase(unittest.TestCase):
     r1 = Experiment.largest([-1, -2, -3, -4])
     self.assertEqual(r1, -1)
 
-#  def test_t3(self):
-#    self.assertRaises(ValueError, Experiment.largest, [])
+  def test_t3(self):
+    self.assertRaises(ValueError, Experiment.largest, [])
 
 if __name__ == '__main__':
   unittest.main()
